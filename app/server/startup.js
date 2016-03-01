1b0fc347c0b08618fc662e5c902450b0b204749d
Emiliano Viada <emiliano.viada@excedesoft.com>
2016-03-01 00:17:57 -0300

    Commenting out startup initialization

--- app/server/startup.js
+++ app/server/startup.js
@@ -1,5 +1,5 @@
 Meteor.startup(function () {
   	// code to run on server at startup
   	// Mailgun config
-	process.env.MAIL_URL = Meteor.settings.MAIL_URL;
+	// process.env.MAIL_URL = Meteor.settings.MAIL_URL;
 });
